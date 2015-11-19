09a9552fb57d5de0beda17747a5ae61d97ce5aae
Kenton Hamaluik <kenton@hamaluik.com>
2015-11-18 22:35:45 -0700

    Added Mat3

---
+++ src/glm/Mat3.hx
@@ -0,0 +1,164 @@
+package glm;
+
+/**
+ * Utility class for dealing with 4x4 matrices
+ */
+abstract Mat3(Array<glm.Vec3>) {
+	public function new(scale:Float = 0) {
+		var arr:Array<glm.Vec3> = [
+			new Vec3(scale, 0, 0),
+			new Vec3(0, scale, 0),
+			new Vec3(0, 0, scale)
+		];
+		this = arr;
+		return cast this;
+	}
+
+	/**
+	 * Construct a Mat3 from a series of Vec3s (representing rows)
+	 * @param  rows<Vec3> the rows
+	 * @return            a Mat3
+	 */
+	public static function fromRowArray(rows:Array<Vec3>):Mat3 {
+		if(rows.length != 3) {
+			throw "You must supply 3 Vec3s to build a Mat3 this way!";
+		}
+		var m:Mat3 = new Mat3();
+		m[0] = rows[0];
+		m[1] = rows[1];
+		m[2] = rows[2];
+		return m;
+	}
+
+	/**
+	 * Construct a Mat3 from a series of Vec3s (representing rows)
+	 * @param  a row 0
+	 * @param  b row 1
+	 * @param  c row 2
+	 * @return   a Mat3
+	 */
+	public static function fromRows(a:Vec3, b:Vec3, c:Vec3):Mat3 {
+		var m:Mat3 = new Mat3();
+		m[0] = a;
+		m[1] = b;
+		m[2] = c;
+		return m;
+	}
+
+	/**
+	 * Makes every element in `this` `=0`
+	 * @return `this`
+	 */
+	public function zero():Mat3 {
+		this[0].zero();
+		this[1].zero();
+		this[2].zero();
+		return cast this;
+	}
+
+	/**
+	 * Creates a new Mat3 where the elements exactly equal the elements of `this`
+	 */
+	public function clone():Mat3 {
+		var copy:Mat3 = new Mat3();
+		copy[0] = this[0].clone();
+		copy[1] = this[1].clone();
+		copy[2] = this[2].clone();
+		return copy;
+	}
+
+	/**
+	 * Makes `this` and identity matrix
+	 * @return `this`
+	 */
+	public function identity():Mat3 {
+		this[0].set(1, 0, 0);
+		this[1].set(0, 1, 0);
+		this[2].set(0, 0, 1);
+		return cast this;
+	}
+
+	/**
+	 * Provides array access in the form of `mat[i]` where `i ∈ [0, 1, 2]`
+	 * @return a `Vec3` representing the `i`th row
+	 */
+	@:arrayAccess public inline function arrayGet(i:Int):Vec3 {
+		return this[i];
+	}
+
+	/**
+	 * Provides array access in the form of `mat[i] = x` where `i ∈ [0, 1, 2]`
+	 * @return a `Vec3` representing the `i`th row (which has been set)
+	 */
+	@:arrayAccess public inline function arraySet(i:Int, x:Vec3):Vec3 {
+		return this[i] = x;
+	}
+
+	/**
+	 * Flattens `this` into [Row-major](https://en.wikipedia.org/wiki/Row-major_order) order
+	 */
+	public function toArrayRowMajor():Array<Float> {
+		return this[0].toArray().concat(
+			this[1].toArray().concat(
+				this[2].toArray()
+			)
+		);
+	}
+
+	/**
+	 * Flattens `this` into [Column-major](https://en.wikipedia.org/wiki/Column-major_order) order
+	 */
+	public function toArrayColMajor():Array<Float> {
+		var ret:Array<Float> = new Array<Float>();
+		for(j in 0...3) {
+			for(i in 0...3) {
+				ret.push(this[i][j]);
+			}
+		}
+		return ret;
+	}
+
+	/**
+	 * Multiplies `b` by `this`
+	 * @param  b The vec4 to multiply by
+	 * @return   `this` ✕ `b`
+	 */
+	public function multVec3(b:Vec3):Vec3 {
+		return new Vec3(
+			this[0][0] * b[0] + this[0][1] * b[1] + this[0][2] * b[2],
+			this[1][0] * b[0] + this[1][1] * b[1] + this[1][2] * b[2],
+			this[2][0] * b[0] + this[2][1] * b[1] + this[2][2] * b[2]
+		);
+	}
+
+	/**
+	 * Allows multiplying matrices and vectors
+	 */
+	@:op(A * B)
+	public static inline function multiplyVec3Op(a:Mat3, b:Vec3):Vec3 {
+		return a.multVec3(b);
+	}
+
+	/**
+	 * Multiplies `b` by `this`
+	 * @param  b The mat4 to multiply by
+	 * @return   `this` ✕ `b`
+	 */
+	public inline function multMat3(b:Mat3):Mat3 {
+		var rows:Array<Vec3> = [
+			this[0][0] * b[0] + this[0][1] * b[1] + this[0][2] * b[2],
+			this[1][0] * b[0] + this[1][1] * b[1] + this[1][2] * b[2],
+			this[2][0] * b[0] + this[2][1] * b[1] + this[2][2] * b[2]
+		];
+		this = rows;
+		return cast this;
+	}
+
+	/**
+	 * Allows multiplying two matrices
+	 */
+	@:op(A * B)
+	public static inline function multiplyMat3Op(a:Mat3, b:Mat3):Mat3 {
+		return a.multMat3(b);
+	}
+}
---
+++ test/glm/TestMat3.hx
@@ -0,0 +1,130 @@
+package glm;
+
+import buddy.*;
+import glm.Mat3;
+using buddy.Should;
+
+class TestMat3 extends BuddySuite {
+	public function new() {
+		describe('Using Mat3', {
+			var ma:Mat3;
+			before({
+				ma = new Mat3();
+			});
+
+			it('should be able to clone itself', {
+				ma = Mat3.fromRows(
+					new Vec3(1, 2, 3),
+					new Vec3(4, 5, 6),
+					new Vec3(7, 8, 9)
+				);
+				var mb:Mat3 = ma.clone();
+				for(i in 0...3) {
+					for(j in 0...3) {
+						mb[i][j].should.beCloseTo(ma[i][j]);
+					}
+				}
+				ma[0][0] = 7;
+				mb[0][0].should.beCloseTo(1);
+			});
+			it('should be able to create a zero matrix', {
+				ma.zero();
+				for(i in 0...3) {
+					for(j in 0...3) {
+						ma[i][j].should.beCloseTo(0);
+					}
+				}
+			});
+			it('should be able to create an identity matrix', {
+				ma.identity();
+				for(i in 0...3) {
+					for(j in 0...3) {
+						ma[i][j].should.beCloseTo(i == j ? 1 : 0);
+					}
+				}
+			});
+			it('should allow array access', {
+				ma = new Mat3(4.2);
+				ma[0][0].should.beCloseTo(4.2);
+			});
+			it('should be able to multliply vec3s with itself', {
+				ma = Mat3.fromRows(
+					new Vec3(1, 2, 3),
+					new Vec3(4, 5, 6),
+					new Vec3(7, 8, 9)
+				);
+				var r:Vec3 = ma * new Vec3(1, 1, 1);
+				r[0].should.beCloseTo(6);
+				r[1].should.beCloseTo(15);
+				r[2].should.beCloseTo(24);
+			});
+			it('should be able to multliply mat4s with itself', {
+				ma = Mat3.fromRows(
+					new Vec3(1, 2, 3),
+					new Vec3(4, 5, 6),
+					new Vec3(7, 8, 9)
+				);
+				var mb:Mat3 = Mat3.fromRows(
+					new Vec3(7, 8, 9),
+					new Vec3(4, 5, 6),
+					new Vec3(1, 2, 3)
+				);
+				var mc = ma * mb;
+				var values:Array<Float> = [18, 24, 30, 54, 69, 84, 90, 114, 138];
+				for(i in 0...3) {
+					for(j in 0...3) {
+						mc[i][j].should.beCloseTo(values[(i * 3) + j]);
+					}
+				}
+			});
+			it('should flatten into row-major order', {
+				ma = Mat3.fromRows(
+					new Vec3(1, 2, 3),
+					new Vec3(4, 5, 6),
+					new Vec3(7, 8, 9)
+				);
+				var actual:Array<Float> = ma.toArrayRowMajor();
+				var expected:Array<Float> = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+				for(i in 0...9) {
+					actual[i].should.beCloseTo(expected[i]);
+				}
+			});
+			it('should flatten into column-major order', {
+				ma = Mat3.fromRows(
+					new Vec3(1, 2, 3),
+					new Vec3(4, 5, 6),
+					new Vec3(7, 8, 9)
+				);
+				var actual:Array<Float> = ma.toArrayColMajor();
+				var expected:Array<Float> = [1, 4, 7, 2, 5, 8, 3, 6, 9];
+				for(i in 0...9) {
+					actual[i].should.beCloseTo(expected[i]);
+				}
+			});
+			it('should be serializable', {
+				ma = Mat3.fromRowArray([
+					new Vec3(1, 2, 3),
+					new Vec3(4, 5, 6),
+					new Vec3(7, 8, 9)
+				]);
+
+				var s:haxe.Serializer = new haxe.Serializer();
+				s.serialize(ma);
+				var serialized:String = s.toString();
+
+				var u:haxe.Unserializer = new haxe.Unserializer(serialized);
+				var mb:Mat3 = u.unserialize();
+				
+				for(i in 0...3) {
+					for(j in 0...3) {
+						mb[i][j].should.beCloseTo(ma[i][j]);
+					}
+				}
+			});
+
+			after({
+				ma = null;
+			});
+		});
+	}
+}
