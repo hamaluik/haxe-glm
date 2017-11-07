1899fbc6ea8f8d528ed94465d26041806c45d6ae
Kenton Hamaluik <hamaluik@ualberta.ca>
2017-11-07 12:54:16 -0700

    Added tests for vectors

--- test/glm/TestVec2.hx
+++ test/glm/TestVec2.hx
@@ -16,23 +16,72 @@ class TestVec2 extends BuddySuite {
 				vb = new Vec2(-1, -2);
 			});
 
-			it("should provide element access");
+			it("should provide element access", {
+				va[1].should.be(2);
+				va.x.should.be(1);
+				va.x = 42;
+				va[0].should.be(42);
+			});
 
-			it("should calculate the magnitude");
+			it("should calculate the magnitude", {
+				var m:Float = Math.sqrt(va.x*va.x + va.y*va.y);
+				va.length().should.beCloseTo(m);
+			});
 
-			it("should provide copy support");
+			it("should provide copy support", {
+				var vc:Vec2 = va.copy(new Vec2());
+				vc.x.should.beCloseTo(va.x);
+				vc.y.should.beCloseTo(va.y);
+			});
 
-			it("should provide vector math");
+			it("should provide vector math", {
+				va.addVec(new Vec2(1, 1), vb);
+				vb.equals(new Vec2(2, 3)).should.be(true);
 
-			it("should provide scalar math");
+				vb.subtractVec(new Vec2(1, 1), vb);
+				vb.equals(va).should.be(true);
 
-			it("should calculate distance between vectors");
+				var vc:Vec2 = va + new Vec2(1, 1);
+				vc.equals(new Vec2(2, 3)).should.be(true);
 
-			it("should calculate the dot product");
+				vc = va - new Vec2(1, 2);
+				vc.equals(new Vec2()).should.be(true);
+			});
 
-			it("should normalize");
+			it("should provide scalar math", {
+				va.addScalar(1, vb);
+				vb.equals(new Vec2(2, 3)).should.be(true);
+				va.multiplyScalar(0.5, vb);
+				vb.equals(new Vec2(0.5, 1)).should.be(true);
 
-			it("should linearly interpolate");
+				var vc:Vec2 = va * 3;
+				vc.equals(new Vec2(3, 6)).should.be(true);
+
+				vc = va + 3;
+				vc.equals(new Vec2(4, 5)).should.be(true);
+
+				vc = va - 1;
+				vc.equals(new Vec2(0, 1)).should.be(true);
+			});
+
+			it("should calculate distance between vectors", {
+				va.distance(vb).should.beCloseTo(4.4721);
+			});
+
+			it("should calculate the dot product", {
+				va.dot(vb).should.beCloseTo(-5);
+			});
+
+			it("should normalize", {
+				var v:Vec2 = va.normalize(new Vec2());
+				v.x.should.beCloseTo(0.4472);
+				v.y.should.beCloseTo(0.8944);
+			});
+
+			it("should linearly interpolate", {
+				var x:Vec2 = va.lerp(vb, 0.5, new Vec2());
+				x.equals(new Vec2()).should.be(true);
+			});
 
 			it("provide the cross product", {
 				va = new Vec2(1, 0);
--- test/glm/TestVec3.hx
+++ test/glm/TestVec3.hx
@@ -15,23 +15,74 @@ class TestVec3 extends BuddySuite {
 				vb = new Vec3(-1, -2, -3);
 			});
 
-			it("should provide element access");
+			it("should provide element access", {
+				va[1].should.be(2);
+				va.x.should.be(1);
+				va.x = 42;
+				va[0].should.be(42);
+			});
 
-			it("should calculate the magnitude");
+			it("should calculate the magnitude", {
+				var m:Float = Math.sqrt(va.x*va.x + va.y*va.y + va.z*va.z);
+				va.length().should.beCloseTo(m);
+			});
 
-			it("should provide copy support");
+			it("should provide copy support", {
+				var vc:Vec3 = va.copy(new Vec3());
+				vc.x.should.beCloseTo(va.x);
+				vc.y.should.beCloseTo(va.y);
+				vc.z.should.beCloseTo(va.z);
+			});
 
-			it("should provide vector math");
+			it("should provide vector math", {
+				va.addVec(new Vec3(1, 1, 1), vb);
+				vb.equals(new Vec3(2, 3, 4)).should.be(true);
 
-			it("should provide scalar math");
+				vb.subtractVec(new Vec3(1, 1, 1), vb);
+				vb.equals(va).should.be(true);
 
-			it("should calculate distance between vectors");
+				var vc:Vec3 = va + new Vec3(1, 1, 1);
+				vc.equals(new Vec3(2, 3, 4)).should.be(true);
 
-			it("should calculate the dot product");
+				vc = va - new Vec3(1, 2, 3);
+				vc.equals(new Vec3()).should.be(true);
+			});
 
-			it("should normalize");
+			it("should provide scalar math", {
+				va.addScalar(1, vb);
+				vb.equals(new Vec3(2, 3, 4)).should.be(true);
+				va.multiplyScalar(0.5, vb);
+				vb.equals(new Vec3(0.5, 1, 1.5)).should.be(true);
 
-			it("should linearly interpolate");
+				var vc:Vec3 = va * 3;
+				vc.equals(new Vec3(3, 6, 9)).should.be(true);
+
+				vc = va + 3;
+				vc.equals(new Vec3(4, 5, 6)).should.be(true);
+
+				vc = va - 1;
+				vc.equals(new Vec3(0, 1, 2)).should.be(true);
+			});
+
+			it("should calculate distance between vectors", {
+				va.distance(vb).should.beCloseTo(7.4833);
+			});
+
+			it("should calculate the dot product", {
+				va.dot(vb).should.beCloseTo(-14);
+			});
+
+			it("should normalize", {
+				var v:Vec3 = va.normalize(new Vec3());
+				v.x.should.beCloseTo(0.2673);
+				v.y.should.beCloseTo(0.5345);
+				v.z.should.beCloseTo(0.8018);
+			});
+
+			it("should linearly interpolate", {
+				var x:Vec3 = va.lerp(vb, 0.5, new Vec3());
+				x.equals(new Vec3()).should.be(true);
+			});
 
 			it("provide the cross product", {
 				va = new Vec3(1, 0, 0);
